effbad243c45ea4a0929fc54e50be35cb0b88a95
Apprenti-Dev-Wiz <japprendslecode@gmail.com>
2024-09-14 12:35:54 +0200

    Add files via upload
    Mon 1er programme

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+a = input("Quel table de mutiplication veux tu ? ")
+a = int(a)
+b = 1
+print("Debut de la table")
+while b <= 24:
+    b = b + 1
+    c = a * b
+    print(str(b) + "x"+ str(a)+"="+ str(c))
+print("fin de la table ")
+
